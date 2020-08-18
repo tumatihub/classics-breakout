409644f30264fca05ecae0ff1ebed7fdeb4106e0
tumatihub <jgsoaresdias@gmail.com>
2020-08-18 16:01:35 -0300

    Change border to trigger

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    [SerializeField] private Vector2 _normal;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            collision.gameObject.GetComponent<BallMovement>().BounceBall(_normal);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(_normal.x, _normal.y) * 2f);
+    }
+}
