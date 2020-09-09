3364d3f6936cdb3574aebc6af1bee1d5edf0e273
tumatihub <jgsoaresdias@gmail.com>
2020-09-09 20:29:24 -0300

    Basic main screen

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNames
+{
+    public const string Game = "Sandbox";
+}
+
+public class SceneController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneNames.Game);
+    }
+}
